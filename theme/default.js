b928685734c60cedf08b28be465ad7a8465d700d
pedroruizf <pedroruizf@gmail.com>
2025-11-18 20:55:06 +0100

    revisión 4

--- theme/default.js
+++ theme/default.js
@@ -43,7 +43,6 @@ var myTheme = {
                         myTheme.params('add');
                     }
                 }
-                window.scroll(0, 0);
             } else {
                 $('body').toggleClass('siteNav-off');
                 myTheme.params(
@@ -62,7 +61,6 @@ var myTheme = {
                 }
                 bar.show();
                 $('#exe-client-search-text').focus();
-                window.scroll(0, 0);
             }
         });
         if (!this.inIframe()) {
@@ -86,21 +84,11 @@ var myTheme = {
                 $(this).text(t).addClass('package-link');
             }
         });
-        $('.page-header .page-title').each(function () {
-            var t = $(this).text().trim();
-            var res;
-            if (t.startsWith('- ') && t.endsWith(' -')) {
-                res = t;
-                res = res.substring(2);
-                res = res.substring(0, res.length - 2);
-                // Update page title
-                $(this).text(res);
-                // Update HTML title
-                document.title = document.title.replace(t, res);
-            }
-        });
         // Search form
         this.searchForm();
+
+        // mover .page-title dentro de .page-content
+        this.movePageTitle();
     },
     inIframe: function () {
         try {
@@ -144,10 +132,43 @@ var myTheme = {
             myTheme.param(this, act);
         });
     },
+
+    // function that move the h2 outside the header
+    movePageTitle: function () {
+        const tryMove = () => {
+            const $header = $('.page > header.page-header');
+            const $title = $header.find('.page-title').first();
+
+            // Search container of content
+            let $content = $('.page-content').first();
+            if (!$content.length)
+                $content = $('.content, main .content').first();
+            if (!$content.length) $content = $('#main, #content').first();
+            if (!$content.length && $header.length)
+                $content = $header.nextAll(':not(header)').first();
+            if (!$content.length && $header.length) $content = $header.parent();
+
+            if ($header.length && $title.length && $content.length) {
+                $content.prepend($title); // move it to the start
+                return true;
+            }
+            return false;
+        };
+
+        if (tryMove()) return;
+
+        const observer = new MutationObserver(() => {
+            if (tryMove()) observer.disconnect();
+        });
+        observer.observe(document.body, { childList: true, subtree: true });
+    },
+    // 🔼
 };
+
 $(function () {
     myTheme.init();
 });
+
 $.fn.isInViewport = function () {
     var elementTop = $(this).offset().top;
     var elementBottom = elementTop + $(this).outerHeight();
